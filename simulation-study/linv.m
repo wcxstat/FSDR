807e96e425addb07f5b9fe52488c498f298a4537
wcxstat <78290096+wcxstat@users.noreply.github.com>
2026-01-30 14:11:45 +0800

    Delete simulation-study directory

--- simulation-study/linv.m
+++
@@ -1,9 +0,0 @@
-function bhat=linv(arg,kappa,eigfun,psi,sn)
-% This is a program to calculate Gamma^(-1)*g
-
-d=size(psi,2);
-bhat=zeros(length(arg),d);
-for k=1:d
-    g=trapz(arg,repmat(psi(:,k),1,sn).*eigfun(:,1:sn));
-    bhat(:,k)=eigfun(:,1:sn)*(g./kappa(1:sn))';
-end
--- simulation-study/trapz2.m
+++
@@ -1,7 +0,0 @@
-function intg=trapz2(z,x,y);
-temp=[];
-n1=length(x);
-for i=1:n1
-    temp(i)=trapz(y,z(i,:));
-end
-intg=trapz(x,temp);
